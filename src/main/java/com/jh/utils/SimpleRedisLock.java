6dd34506c6c3e6dfc11310db82a9e0a41184acae
jh790074213 <jh790074213@163.com>
2024-07-10 17:18:18 +0800

    解决分布式锁原子性问题：添加锁标识，防止误删                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               =

--- src/main/java/com/jh/utils/SimpleRedisLock.java
+++ src/main/java/com/jh/utils/SimpleRedisLock.java
@@ -1,6 +1,8 @@
 package com.jh.utils;
 
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
+
 
 import java.util.concurrent.TimeUnit;
 
@@ -8,6 +10,8 @@ import java.util.concurrent.TimeUnit;
 public class SimpleRedisLock implements ILock {
     private String name;
     public static final String KEY_PREFIX = "lock:";
+    // 对每个JVM生成一份UUID，在同一个JVM下线程id是唯一，但是跨JVM不一定是唯一，因为是JVM给线程生成的递增数字
+    public static final String ID_PREFIX = UUID.randomUUID().toString(true)+"-";
     private final StringRedisTemplate stringRedisTemplate;
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
@@ -17,16 +21,20 @@ public class SimpleRedisLock implements ILock {
 
     @Override
     public boolean tryLock(long timeoutSec) {
-        // 获取锁
-        long threadId = Thread.currentThread().getId();
+        // 获取锁,使用UUID和线程id作为锁唯一标识
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
         Boolean success = stringRedisTemplate.opsForValue()
-                .setIfAbsent(KEY_PREFIX + name, threadId + "", timeoutSec, TimeUnit.SECONDS);
+                .setIfAbsent(KEY_PREFIX + name, threadId, timeoutSec, TimeUnit.SECONDS);
         // 自动拆箱可能会空指针，当success内部是null时
         return Boolean.TRUE.equals(success);
     }
 
     @Override
     public void unlock() {
-        stringRedisTemplate.delete(KEY_PREFIX + name);
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+        if(threadId.equals(id)) {
+            stringRedisTemplate.delete(KEY_PREFIX + name);
+        }
     }
 }
