e9fecc33e01a0c27a1e04bdf1e10586642b8462f
jh790074213 <jh790074213@163.com>
2024-07-11 19:07:44 +0800

    基于Lua脚本实现分布式锁的释放锁逻辑                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              =

--- src/main/java/com/jh/utils/SimpleRedisLock.java
+++ src/main/java/com/jh/utils/SimpleRedisLock.java
@@ -1,9 +1,13 @@
 package com.jh.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
 
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 
@@ -13,6 +17,13 @@ public class SimpleRedisLock implements ILock {
     // 对每个JVM生成一份UUID，在同一个JVM下线程id是唯一，但是跨JVM不一定是唯一，因为是JVM给线程生成的递增数字
     public static final String ID_PREFIX = UUID.randomUUID().toString(true)+"-";
     private final StringRedisTemplate stringRedisTemplate;
+    // 初始化Lua脚本
+    public static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -28,13 +39,20 @@ public class SimpleRedisLock implements ILock {
         // 自动拆箱可能会空指针，当success内部是null时
         return Boolean.TRUE.equals(success);
     }
-
     @Override
     public void unlock() {
-        String threadId = ID_PREFIX + Thread.currentThread().getId();
-        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-        if(threadId.equals(id)) {
-            stringRedisTemplate.delete(KEY_PREFIX + name);
-        }
+        // 调用Lua脚本
+        stringRedisTemplate.execute(UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId());
+
     }
+    // @Override
+    // public void unlock() {
+    //     String threadId = ID_PREFIX + Thread.currentThread().getId();
+    //     String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+    //     if(threadId.equals(id)) {
+    //         stringRedisTemplate.delete(KEY_PREFIX + name);
+    //     }
+    // }
 }
