bcde30188a600aa054ed2840af685c4c2ba7deed
jh790074213 <jh790074213@163.com>
2024-07-18 16:10:47 +0800

    完成发布笔记和查看笔记功能                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       =

--- src/main/java/com/jh/config/RedissonConfig.java
+++ src/main/java/com/jh/config/RedissonConfig.java
@@ -12,7 +12,7 @@ public class RedissonConfig {
     @Bean
     public RedissonClient redissonClient() {// 配置
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://114.116.225.208").setPassword("@jh987654321");// 创建RedissonClient对象
+        config.useSingleServer().setAddress("redis://114.116.225.208:6379").setPassword("@jh987654321");// 创建RedissonClient对象
         return Redisson.create(config);
     }
 }
--- src/main/java/com/jh/service/IBlogService.java
+++ src/main/java/com/jh/service/IBlogService.java
@@ -1,5 +1,6 @@
 package com.jh.service;
 
+import com.jh.dto.Result;
 import com.jh.entity.Blog;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,4 +14,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IBlogService extends IService<Blog> {
 
+    Result queryHotBlog(Integer current);
+
+    Result queryBlogById(Long id);
 }
